9e8e2e06b32e9b96490303b3f73f631ac42e4c94
Ricardo Carrasco Cursach <rccursach@gmail.com>
2017-12-18 21:25:22 -0300

    add libs bars and filter

--- src/main.js
+++ src/main.js
@@ -3,6 +3,11 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import Bars from 'vuebars'
+import Vue2Filters from 'vue2-filters'
+
+Vue.use(Bars)
+Vue.use(Vue2Filters)
 
 window.$ = window.jQuery = require('jquery')
 require('semantic-ui-css/semantic.css')
